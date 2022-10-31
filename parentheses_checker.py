30eb9927b8620a07fca878e06faf4e5a6d75fc57
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2022-10-31 22:46:16 +0200

    Solving corner cases.

--- parentheses_checker.py
+++ parentheses_checker.py
@@ -5,22 +5,27 @@ s = Stack()
 
 def parentheses_checker(input_string):
     if len(input_string) == 0:
-        print("Please enter a valid string.")
-    else:
-        for i in range(len(input_string)):
-            if s.is_empty():
-                s.push(input_string[i])
-            else:
-                tmp = s.peek()
-                if tmp == input_string[i]:
-                    s.push(input_string[i])
-                else:
-                    s.pop()
-        if s.is_empty():
-            return print("The string is balanced.")
+        print("Please enter a non empty string.")
+    for c in input_string:
+        if s.is_empty() and c == '(':
+            s.push(c)
+        elif s.is_empty():
+            print("The input string is invalid")
+            return
+        elif c == s.peek():
+            s.push(c)
         else:
-            return print("The string is not balanced.")
+            s.pop()
+
+    if s.is_empty():
+        return True
+    else:
+        return False
 
 
 usrStr = input("Please enter your parentheses string: ")
-parentheses_checker(usrStr)
+if parentheses_checker(usrStr):
+    print("The parentheses string is balanced.")
+else:
+    print("The parentheses string is NOT balanced.")
+
